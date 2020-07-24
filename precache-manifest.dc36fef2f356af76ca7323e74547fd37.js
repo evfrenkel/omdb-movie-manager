848d0b2867b6ea5ac2d4be0fe188df401ff9852f
Evan Frenkel <evan.frenkel@lendinghome.com>
2020-07-24 16:11:40 -0700

    Updates

---
+++ precache-manifest.dc36fef2f356af76ca7323e74547fd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7213bb0c062f59de59c8d32984ea06c",
+    "url": "/omdb-movie-manager/index.html"
+  },
+  {
+    "revision": "81e22f427383e9bb982a",
+    "url": "/omdb-movie-manager/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "81e22f427383e9bb982a",
+    "url": "/omdb-movie-manager/static/js/2.43c17bcb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/omdb-movie-manager/static/js/2.43c17bcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aec4c7f5a16f28a0e31",
+    "url": "/omdb-movie-manager/static/js/main.56dd3453.chunk.js"
+  },
+  {
+    "revision": "3bffb7e54de900bb8df3",
+    "url": "/omdb-movie-manager/static/js/runtime-main.7a845def.js"
+  }
+]);
